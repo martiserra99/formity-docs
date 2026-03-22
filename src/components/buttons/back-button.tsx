4b7f728ec4d3c608f918cafbecf098245f1ff744
Marti Serra <martiserramolina99@gmail.com>
2026-03-22 11:50:14 +0100

    Add types in back button

--- src/components/buttons/back-button.tsx
+++ src/components/buttons/back-button.tsx
@@ -5,9 +5,11 @@ import { useFormContext } from "react-hook-form";
 import { Button } from "../button";
 import { useMultiStep } from "@/multi-step";
 
-export function BackButton(props: ComponentPropsWithoutRef<"button">) {
-  const { getValues } = useFormContext();
-  const { onBack } = useMultiStep();
+export function BackButton<T extends Record<string, unknown>>(
+  props: ComponentPropsWithoutRef<"button">,
+) {
+  const { getValues } = useFormContext<T>();
+  const { onBack } = useMultiStep<T>();
   return (
     <Button
       type="button"
